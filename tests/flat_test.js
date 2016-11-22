a6da4963c7af783803822c523cf6c1fa3a821dfe
Aleks <info@aleks.bz>
2016-11-22 21:44:57 +0200

    Tests

--- tests/flat_test.js
+++ tests/flat_test.js
@@ -8,4 +8,16 @@ describe('Flat test', function(){
 
         flat.flatter(arr).sort().should.eql(comp.sort());
     });
+    it('should flatter with some null values', function(){
+        var arr = [[1,2,[],4],5, [6, null]];
+        var comp = [1, 2, 4, 5, 6];
+
+        flat.flatter(arr).sort().should.eql(comp.sort());
+    });
+    it('should flatter with zero values', function(){
+        var arr = [0, [1,2,[],4],5, [6, null]];
+        var comp = [0, 1, 2, 4, 5, 6];
+
+        flat.flatter(arr).sort().should.eql(comp.sort());
+    });
 });
